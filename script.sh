6c20ab0b07c13f3a2c4aaa4ca4fc7b8070b27936
DtyaaAjh <100247109+Dityay@users.noreply.github.com>
2024-10-17 17:57:01 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen --git-lfs --depth=1 
+repo init -u https://github.com/CipherOS/android_manifest.git -b fifteen --git-lfs --depth=1 
  git clone https://github.com/Dityay/Local-Manifest --depth 1 -b Voltage-14 .repo/local_manifests && 
 # Sync the repositories
 /opt/crave/resync.sh  && 
@@ -7,8 +7,7 @@ repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen --git-
 export BUILD_USERNAME=Dityay 
  export BUILD_HOSTNAME=crave 
  source build/envsetup.sh && 
-echo Repository: Dityay/crave_aosp_builder
- echo Run ID: 11340279996
+echo Building...
  
 # Build the ROM
 breakfast earth userdebug && make installclean && mka bacon -j4
