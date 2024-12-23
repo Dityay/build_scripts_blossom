52c4fa1dfdd4279904331d65f692068ebdb5986f
Dtyaa <100247109+Dityay@users.noreply.github.com>
2024-12-23 14:23:23 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init -u https://github.com/SkylineUI/manifest -b aosp-14 --git-lfs
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
- git clone https://github.com/Dityay/Local-Manifest --depth 1 -b Voltage-14 .repo/local_manifests &&
+ git clone https://github.com/Dityay/android_device_xiaomi_earth_qpr2 device/xiaomi/earth &&
+ git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth vendor/xiaomi/earth &&
+ git clone https://github.com/Dityay/android_kernel_xiaomi_earth kernel/xiaomi/earth &&
  git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr &&
+ git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi &&
+ git clone https://github.com/LineageOS/android_hardware_mediatek hardware/mediatek &&
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
